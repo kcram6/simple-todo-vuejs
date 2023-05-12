e560fdb52b85e9135d0b35f852732f52d7055bc4
Kathleen Cram <d00297334@dmail.dixie.edu>
2023-05-12 08:25:28 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        title: "Todo List",
+        newTodo: "",
+        todoList: []
+    },
+    methods: {
+        addTodo() {
+            console.log(this.newTodo);
+            this.todoList.push({
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo = "";
+        },
+        removeTodo(todo) {
+            const todoIndex = this.todoList.indexOf(todo);
+            this.todoList.splice(todoIndex, 1);
+        },
+        allDone() {
+            this.todoList.forEach(todo => {
+                todo.done = true;
+                
+            });
+        }
+    }
+});
